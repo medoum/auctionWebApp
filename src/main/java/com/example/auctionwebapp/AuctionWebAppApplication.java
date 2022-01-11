1c13e091ea1cc87014d1081fde90ef65fe3f1141
medoum <mohamedoum2430@gmail.com>
2022-01-11 23:42:19 +0100

    Initial commit

---
+++ src/main/java/com/example/auctionwebapp/AuctionWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.auctionwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/auctionwebapp/AuctionWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auctionwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
